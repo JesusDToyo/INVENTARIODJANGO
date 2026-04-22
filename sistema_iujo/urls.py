0d4d3e0598e771accf849a6e3601c59f489f6308
JesusDToyo <jdtoyod@gmail.com>
2026-04-22 16:53:02 -0400

    Templates + inventario

--- sistema_iujo/urls.py
+++ sistema_iujo/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from inventario import views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),  
+    path('', views.lista_productos, name='lista_productos'), 
+    path('nuevo/', views.crear_producto, name='crear_producto'),
 ]
